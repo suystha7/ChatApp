54c22ee94ce7ce6f712ac78e8ac3208150a8da9c
suyogstha7 <suyogstha317@gmail.com>
2025-01-19 22:08:33 +0545

    setup

--- server/app.js
+++ server/app.js
@@ -1,5 +1,54 @@
 const express = require("express");
 
-const morgan = 
+const morgan = require("morgan");
 
-const app = express()
+const rateLimit = require("express-rate-limit");
+
+const helmet = require("helmet");
+
+const mongoSanitize = require("express-mongo-sanitize");
+
+const bodyParser = require("body-parser");
+
+const xss = require("xss");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(mongoSanitize());
+// app.use(xss());
+
+app.use(express.json({ limit: "10kb" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const limiter = rateLimit({
+  max: 3000,
+  windowMs: 60 * 60 * 1000,
+  message: "TOo many request from this IP, Please try again in an hour",
+});
+
+app.use("/talk", limiter);
+
+
+module.exports = app;
