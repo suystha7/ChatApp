cb423194089f5f9d71012b4eab7876acd280001e
suyogstha7 <suyogstha317@gmail.com>
2025-01-23 19:20:52 +0545

    changes

--- client/src/pages/dashboard/GeneralApp.js
+++ client/src/pages/dashboard/GeneralApp.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { ChatHeader } from "../../components/Chat";
+import Footer from "../../components/Chat/Footer";
 
 const GeneralApp = () => {
 
   return (
     <>
-      App
+      <ChatHeader />
+      <Footer />
     </>
   );
 };
